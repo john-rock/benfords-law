aea5c6b195ce208af0901dc7f1c58c78d032d8fd
john-rock <johnmrock.jr@gmail.com>
2020-08-04 20:54:34 -0400

    Get frequencies of first digits

--- src/components/DataForm.js
+++ src/components/DataForm.js
@@ -31,14 +31,37 @@ function DataForm() {
 
         // Get the first digit of each item in the dataSet array.
         const firstDigitArr = results.map(function (item, index, array) {
-            return item.toString()[0]
+            return item.toString()[0];
         });
+
+        var firstDigitFreq = {};
+
+        for (var i = 0; i < firstDigitArr.length; i++) {
+          var num = firstDigitArr[i];
+          firstDigitFreq[num] = firstDigitFreq[num] ? firstDigitFreq[num] + 1 : 1;
+        }
+        
+        const fOne = firstDigitFreq[1]
+        const fTwo = firstDigitFreq[2]
+        const fThree = firstDigitFreq[3]
+        const fFour = firstDigitFreq[4]
+        const fFive = firstDigitFreq[5]
+        const fSix = firstDigitFreq[6]
+        const fSeven = firstDigitFreq[7]
+        const fEight = firstDigitFreq[8]
+        const fNine = firstDigitFreq[9]
 
         console.log(results);
         console.log(firstDigitArr);
-
-        // Regex to remove period and comma from number
-        // .replace(/([.,])(\d\d\d\D|\d\d\d$)/g,'$2')
+        console.log(fOne);
+        console.log(fTwo);
+        console.log(fThree);
+        console.log(fFour);
+        console.log(fFive);
+        console.log(fSix);
+        console.log(fSeven);
+        console.log(fEight);
+        console.log(fNine);
         
       };
 
