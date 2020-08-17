1f004b3a3b393129c1a0c546bdc9a62f631fb7d1
John Rock <johnmrock.jr@gmail.com>
2020-08-16 21:29:31 -0400

    Mobile overflow fix

--- src/components/Sources.js
+++ src/components/Sources.js
@@ -4,9 +4,9 @@ const Sources = () => {
     return (
         <div className="sources">
             <p>Data set sources:</p>
-            <p><a rel="noopener noreferrer" target="_blank" href="https://www.worldometers.info/coronavirus/">https://www.worldometers.info/coronavirus/</a></p>
-            <p><a rel="noopener noreferrer" target="_blank" href="http://www.atlasoftheuniverse.com/stars.html">http://www.atlasoftheuniverse.com/stars.html</a></p>
-            <p><a rel="noopener noreferrer" target="_blank" href="https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)">https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)</a></p>
+            <a rel="noopener noreferrer" target="_blank" href="https://www.worldometers.info/coronavirus/">https://www.worldometers.info/coronavirus/</a>
+            <a rel="noopener noreferrer" target="_blank" href="http://www.atlasoftheuniverse.com/stars.html">http://www.atlasoftheuniverse.com/stars.html</a>
+            <a rel="noopener noreferrer" target="_blank" href="https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)">https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)</a>
         </div>
     )
 }
