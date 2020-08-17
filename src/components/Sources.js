bf6b2c5ff9406506ff422ee6e5315ff847476040
John Rock <johnmrock.jr@gmail.com>
2020-08-16 21:34:22 -0400

    Fix width on mobile

--- src/components/Sources.js
+++ src/components/Sources.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Sources = () => {
     return (
-        <div className="sources">
-            <p>Data set sources:</p>
-            <a rel="noopener noreferrer" target="_blank" href="https://www.worldometers.info/coronavirus/">https://www.worldometers.info/coronavirus/</a>
-            <a rel="noopener noreferrer" target="_blank" href="http://www.atlasoftheuniverse.com/stars.html">http://www.atlasoftheuniverse.com/stars.html</a>
-            <a rel="noopener noreferrer" target="_blank" href="https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)">https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)</a>
-        </div>
+        <>
+            <div className="sources">
+                <p>Data set sources:</p>
+                <a rel="noopener noreferrer" target="_blank" href="https://www.worldometers.info/coronavirus/">https://www.worldometers.info/coronavirus/</a>
+                <a rel="noopener noreferrer" target="_blank" href="http://www.atlasoftheuniverse.com/stars.html">http://www.atlasoftheuniverse.com/stars.html</a>
+                <a rel="noopener noreferrer" target="_blank" href="https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)">https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)</a>
+            </div>
+        </>
     )
 }
 
