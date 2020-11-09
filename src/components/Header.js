97df89ed61061f2a8faa6673df687f25e34ec8e4
John Rock <johnmrock.jr@gmail.com>
2020-11-08 19:39:43 -0500

    Change menu items

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import { slide as Menu } from 'react-burger-menu';
+import React from "react";
+import { slide as Menu } from "react-burger-menu";
+import { FaGithub } from 'react-icons/fa';
+import { BsKanban } from 'react-icons/bs';
 
 // import ContactForm from './ContactForm';
 
 const Header = () => {
-    return (
-        <>
-            <div className="header">
-                <div className="header__container">
-                    <h1 className="logo">Benford's Law Calculator</h1>
-                    <Menu right width={'320px'}>
-                        <h2>Thank You.</h2>
-                        <p>
-                            If you have found this tool useful or would like to
-                            suggest a feature please use the <strong>Contact</strong> link below.
-                            </p>
-                            <p>
-                            This tool is open source, if you would like to contribute or view the roadmap visit the <strong>Project Board</strong>.
-                        </p>
-                        {/* <ContactForm /> */}
-                        <div className="menu-links">
-                            <a href="mailto:johnmrock.jr@gmail.com?subject=Benfords Law Calculator Feedback">
-                                Contact
-                            </a>
-                            <a
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                href="https://github.com/john-rock/benfords-law/projects/2?fullscreen=true"
-                            >
-                                Project Board
-                            </a>
-                        </div>
-                    </Menu>
-                </div>
+  return (
+    <>
+      <div className="header">
+        <div className="header__container">
+          <h1 className="logo">Benford's Law Calculator</h1>
+          <Menu right width={"320px"}>
+            <h2>Thank You.</h2>
+            <p>I hope you have found this tool helpful.</p>
+            <p>Visit us on Github to contribute or view our project roadmap.</p>
+            {/* <ContactForm /> */}
+            <div className="menu-links">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/john-rock/benfords-law"
+              >
+                <FaGithub /> View on Github
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/john-rock/benfords-law/projects/2?fullscreen=true"
+              >
+                <BsKanban /> Project Roadmap
+              </a>
             </div>
-        </>
-    );
+          </Menu>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
