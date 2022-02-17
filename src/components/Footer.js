83a0d25fdb78a27617a4d73bac8c5e2cd95ea624
John Rock <johnmrock.jr@gmail.com>
2022-02-17 11:12:29 -0500

    Update link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
-        Built with React <span role='img' aria-label="Bolt emoji">⚡</span> by <a href="https://johnrock.me/" target="_blank" rel="noopener">John Rock</a>
+        Built with React <span role='img' aria-label="Bolt emoji">⚡</span> by <a href="https://johnrock.me/" target="_blank" rel="nnoopener noreferrer">John Rock</a>
     </footer>
   )
 }
